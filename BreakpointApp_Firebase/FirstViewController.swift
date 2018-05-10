6e3cbdf44af3eb8372f09275293a44ac4555a6fe
Nguyen Hieu Trung <ktpm11520669@gmail.com>
2018-05-10 10:37:20 +0700

    Initial Commit

---
+++ BreakpointApp_Firebase/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BreakpointApp_Firebase
+//
+//  Created by Nguyen Hieu Trung on 5/10/18.
+//  Copyright © 2018 NHTSOFT. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BreakpointApp_Firebase/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BreakpointApp_Firebase
+//
+//  Created by Nguyen Hieu Trung on 5/10/18.
+//  Copyright © 2018 NHTSOFT. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
